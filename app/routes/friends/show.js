d04100194ad787938483a72a89df286821791700
Chad Ostrowski <hi@chadoh.com>
2015-05-11 21:42:46 -0400

    Implement deleting

--- app/routes/friends/show.js
+++ app/routes/friends/show.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    delete: function(friend) {
+      friend.destroyRecord().then(() => this.transitionTo('friends.index'));
+      return false;
+    }
+  }
 });
